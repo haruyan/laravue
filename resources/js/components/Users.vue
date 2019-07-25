29d39ee84e921084e6e06d7b62e7455f1c91e0e6
haruyan <sufyan.say@gmail.com>
2019-07-25 21:51:47 +0700

    create crud user

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -1,58 +1,132 @@
 <template>
-    <div class="container">
-        <div class="row mt-5">
-          <div class="col-md-12">
-            <div class="card">
-              <div class="card-header">
-                <h3 class="card-title">Users Table</h3>
+  <div class="container">
+      <div class="row mt-5">
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-header">
+              <h3 class="card-title">Users Table</h3>
 
-                <div class="card-tools">
-                  <button class="btn btn-success"><i class="fas fa-user-plus fa-fw"></i> Add User</button>
+              <div class="card-tools">
+                <button class="btn btn-success" data-toggle="modal" data-target="#addNew"><i class="fas fa-user-plus fa-fw"></i> Add User</button>
+              </div>
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body table-responsive p-0">
+              <table class="table table-hover">
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Type</th>
+                    <th>Modify</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>183</td>
+                    <td>John Doe</td>
+                    <td>11-7-2014</td>
+                    <td><span class="tag tag-success">Approved</span></td>
+                    <td>
+                        <a href="#">
+                            <i class="fa fa-edit"></i>
+                        </a>
+                        /
+                        <a href="#">
+                            <i class="fa fa-trash red"></i>
+                        </a>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <!-- /.card-body -->
+          </div>
+          <!-- /.card -->
+        </div>
+      </div>
+
+      <!-- Modal -->
+      <div class="modal fade" id="addNew" tabindex="-1" role="dialog" aria-labelledby="addNewLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="addNewLabel">Add New</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <form @submit.prevent="createUser">
+              <div class="modal-body">
+                <div class="form-group">
+                  <input v-model="form.name" type="text" name="name" placeholder="Name" 
+                    class="form-control" :class="{ 'is-invalid': form.errors.has('name') }">
+                  <has-error :form="form" field="name"></has-error>
+                </div>
+
+                <div class="form-group">
+                  <input v-model="form.email" type="email" name="email" placeholder="Email Address" 
+                    class="form-control" :class="{ 'is-invalid': form.errors.has('email') }">
+                  <has-error :form="form" field="name"></has-error>
+                </div>
+
+                <div class="form-group">
+                  <textarea v-model="form.bio" type="text" name="bio" placeholder="Some bio (optional)" 
+                    class="form-control" :class="{ 'is-invalid': form.errors.has('bio') }"></textarea>
+                  <has-error :form="form" field="name"></has-error>
+                </div>
+
+                <div class="form-group">
+                  <select v-model="form.type" name="type" id="type" 
+                    class="form-control" :class="{ 'is-invalid': form.errors.has('type') }">
+                    <option disabled>--Select user role--</option>
+                    <option value="admin">Admin</option>
+                    <option value="user">Standard User</option>
+                    <option value="author">Author</option>
+                  </select>
+                  <has-error :form="form" field="name"></has-error>
+                </div>
+
+                <div class="form-group">
+                  <input v-model="form.password" type="password" name="password" placeholder="Some bio (optional)" 
+                    class="form-control" :class="{ 'is-invalid': form.errors.has('password') }">
+                  <has-error :form="form" field="name"></has-error>
                 </div>
               </div>
-              <!-- /.card-header -->
-              <div class="card-body table-responsive p-0">
-                <table class="table table-hover">
-                  <thead>
-                    <tr>
-                      <th>ID</th>
-                      <th>Name</th>
-                      <th>Email</th>
-                      <th>Type</th>
-                      <th>Modify</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>183</td>
-                      <td>John Doe</td>
-                      <td>11-7-2014</td>
-                      <td><span class="tag tag-success">Approved</span></td>
-                      <td>
-                          <a href="#">
-                              <i class="fa fa-edit"></i>
-                          </a>
-                          /
-                          <a href="#">
-                              <i class="fa fa-trash red"></i>
-                          </a>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
+              <!-- End Body -->
+              <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Create</button>
               </div>
-              <!-- /.card-body -->
-            </div>
-            <!-- /.card -->
+            </form>
           </div>
         </div>
-    </div>
+      </div>
+  </div>
 </template>
 
 <script>
     export default {
-        mounted() {
-            console.log('Component mounted.')
+      data() {
+        return {
+          form: new Form({
+            name : '',
+            email: '',
+            password: '',
+            type: '',
+            bio: '',
+            photo: ''
+          })
         }
+      },
+      methods: {
+        createUser(){
+          this.form.post('api/user');
+        }
+      },
+      mounted() {
+          console.log('Component mounted.')
+      }
     }
 </script>
